ac39007a05cbb5c462ecef68ae18ab24ef9f059a
chenkw <765518301@qq.com>
2020-06-26 23:07:46 +0800

    这是修改挨app1ä¸下的views文件中，添加的第一个数å视图

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def index(request):
+    print('这是第一个视图')
+
+    return HttpResponse('success')
